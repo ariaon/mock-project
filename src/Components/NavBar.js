15e47ea43421fba7dc3792d5d8c40fb2dbd38b83
Andrew Ton <arton@deloitte.com>
2022-08-08 19:33:44 -0700

    refactor: switched to regular navbar html tag

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,54 +7,63 @@ import UserAvatar from "../Images/UserAvatar.png";
 
 const NavBar = () => {
   return (
-    <div>
-      <div style={styles.container}>
-        <p style={styles.title}>Avion</p>
-        <div style={styles.mainLinks}>
-          <MainLink name="Plant Pots" />
-          <MainLink name="Ceramics" />
-          <MainLink name="Tables" />
-          <MainLink name="Chairs" />
-          <MainLink name="Crockery" />
-          <MainLink name="Tableware" />
-          <MainLink name="Cutlery" />
-        </div>
-        <div style={styles.sideActions}>
+    <navbar>
+      <ul style={styles.navList}>
+        <li style={styles.left}>
+          <p style={styles.title}>Avion</p>
+          <div style={styles.mainLinks}>
+            <MainLink name="Plant Pots" />
+            <MainLink name="Ceramics" />
+            <MainLink name="Tables" />
+            <MainLink name="Chairs" />
+            <MainLink name="Crockery" />
+            <MainLink name="Tableware" />
+            <MainLink name="Cutlery" />
+          </div>
+        </li>
+        <li style={styles.sideActions}>
           <SideAction image={Search} desc="search" />
           <SideAction image={ShoppingCart} desc="shoppingCart" />
           <SideAction image={UserAvatar} desc="userAvatar" />
-        </div>
-      </div>
-    </div>
+        </li>
+      </ul>
+    </navbar>
   );
 };
 
 export { NavBar };
 
 const styles = {
-  container: {
-    display: "grid",
-    gridTemplateColumns: "auto auto auto",
-    width: "100%",
-    paddingTop: 22,
-    paddingBottom: 22,
+  navList: {
+    height: 80,
+    display: "flex",
+    justifyContent: "flex-end",
     alignItems: "center",
+    marginBottom: 0,
+  },
+  left: {
+    display: 'flex',
+    marginRight: 'auto',
+    paddingLeft: 70
   },
   title: {
     ...headline3,
+    marginBottom: 0,
     fontFamily: "Clash Display, sans-serif",
     color: "#22202E",
-    marginBottom: 0,
-    paddingLeft: 80,
   },
   mainLinks: {
     display: "flex",
     gap: 44,
+    marginRight: "auto",
+    paddingLeft: 150,
+    listStyle: "none",
+    alignItems: 'center'
   },
   sideActions: {
     display: "flex",
-    justifyContent: "right",
     paddingRight: 100,
     gap: 10,
+    listStyle: "none",
   },
 };
