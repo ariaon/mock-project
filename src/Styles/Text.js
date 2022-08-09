6f4d58b595b1e525a577dbae3d815d6921cdde2d
Andrew Ton <arton@deloitte.com>
2022-08-08 18:46:21 -0700

    chore: repo structure set up

---
+++ src/Styles/Text.js
@@ -0,0 +1,46 @@
+const headline1 = {
+    fontSize: 36,
+    lineHeight: "150%"
+}
+
+const headline2 = {
+    fontSize: 32,
+    lineHeight: "150%"
+}
+
+const headline3 = {
+    fontSize: 24,
+    lineHeight: "150%"
+}
+
+const headline4 = {
+    fontSize: 20,
+    lineHeight: "150%"
+}
+
+const headline5 = {
+    fontSize: 16,
+    lineHeight: "150%"
+}
+
+const headline6 = {
+    fontSize: 14,
+    lineHeight: "150%"
+}
+
+const bodyS = {
+    fontSize: 14,
+    lineHeight: "150%"
+}
+
+const bodyM = {
+    fontSize: 16,
+    lineHeight: "150%"
+}
+
+const bodyL = {
+    fontSize: 18,
+    lineHeight: "150%"
+}
+
+export { headline1, headline2, headline3, headline4, headline5, headline6, bodyS, bodyM, bodyL }
