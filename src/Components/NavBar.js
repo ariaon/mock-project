9397090e4ff763f30ad831da7ba17db39ac808c7
Andrew Ton <110851941+ariaon@users.noreply.github.com>
2022-08-12 02:46:44 -0700

    feat(ProductPage): created mobile design, added more responsiveness

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,31 +1,48 @@
+import { useState, useEffect } from "react";
 import { MainLink } from "./MainLink";
 import { SideAction } from "./SideAction";
 import Search from "../Images/Search.png";
 import ShoppingCart from "../Images/ShoppingCart.png";
 import UserAvatar from "../Images/UserAvatar.png";
-import '../CSS/NavBar.css'
-import '../CSS/Styles/Text.css'
+import Menu from "../Images/Menu.png"
+import "../CSS/NavBar.css";
+import "../CSS/Styles/Text.css";
+import { breakpointTablet } from "../Utils/breakpoints";
 
 const NavBar = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
   return (
     <nav>
-      <ul className='nav-bar__nav_list'>
+      <ul className="nav-bar__nav_list">
         <li className="nav-bar__left_container">
           <p className="nav-bar__title headline3">Avion</p>
-          <div className="nav-bar__main_links">
-            <MainLink name="Plant Pots" />
-            <MainLink name="Ceramics" />
-            <MainLink name="Tables" />
-            <MainLink name="Chairs" />
-            <MainLink name="Crockery" />
-            <MainLink name="Tableware" />
-            <MainLink name="Cutlery" />
-          </div>
+          {width >= breakpointTablet && (
+            <div className="nav-bar__main_links">
+              <MainLink name="Plant Pots" />
+              <MainLink name="Ceramics" />
+              <MainLink name="Tables" />
+              <MainLink name="Chairs" />
+              <MainLink name="Crockery" />
+              <MainLink name="Tableware" />
+              <MainLink name="Cutlery" />
+            </div>
+          )}
         </li>
         <li className="nav-bar__side_actions">
           <SideAction image={Search} desc="search" />
           <SideAction image={ShoppingCart} desc="shoppingCart" />
           <SideAction image={UserAvatar} desc="userAvatar" />
+          {width < breakpointTablet && (
+            <SideAction image={Menu} desc="menu" />
+          )}
         </li>
       </ul>
     </nav>
